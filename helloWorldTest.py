bb6be53e6c09d92231f8dd0380958440867e2736
lsc-leo <leoscholl@web.de>
2024-05-18 13:23:27 +0200

    Next test

--- helloWorldTest.py
+++ helloWorldTest.py
@@ -1,4 +1,4 @@
 print("helloworld")
 msg = 'This is my message'
-msg2 = "Both apostrophes and this other stuff work"
+msg2 = "Both apostrophes and this other stuff workss"
 print(msg + " - " + msg2)
